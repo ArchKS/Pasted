d3494443436edd996ab9bb487a785edafc61b42d
zen <duzhenquan@eastmoney.com>
2021-08-17 13:02:12 +0800

    init.sh

--- init.sh
+++ init.sh
@@ -1,15 +1,8 @@
-npm install election --save-dev
-npm install --arch=ia32 --platform=win32 electron
+git clone  https://github.com/electron/electron-quick-start 
+cd election-quick-start
+npm install && npm start
 
 # fast in china
-
-npm config set registry https://registry.npm.taobao.org
-cat  >> ~/.npmrc << EOF
-registry=https://registry.npm.taobao.org/
-sass_binary_site=https://npm.taobao.org/mirrors/node-sass/
-phantomjs_cdnurl=http://npm.taobao.org/mirrors/phantomjs
-ELECTRON_MIRROR=http://npm.taobao.org/mirrors/electron/
-EOF
-
-# vertify
-npx election -v 
+export http.proxys=socks5://127.0.0.1:7777
+# unset proxy
+export http.proxys=
